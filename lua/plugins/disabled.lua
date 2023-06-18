f85717f98318cc2baae71876bc52e2c8f0b51905
Heshan <heshan.ua@gmail.com>
2023-06-19 01:43:11 +0530

    Override keymaps and alpha configs

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "ggandor/flit.nvim",
+    enabled = false,
+    keys = {
+      { "s", false },
+      { "S", false },
+      { "t", false },
+      { "T", false },
+      { "f", false },
+      { "F", false },
+    },
+  },
+  {
+    "ggandor/leap.nvim",
+    enabled = false,
+    keys = {
+      { "s", false },
+      { "S", false },
+      { "t", false },
+      { "T", false },
+      { "f", false },
+      { "F", false },
+    },
+  },
+}
